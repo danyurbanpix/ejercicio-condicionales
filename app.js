ae90eb2a03f72ae50fddc6fe11e4797e2c07e150
danyurbanpix <danielalonso_67@hotmail.com>
2022-06-05 11:25:54 -0300

    Estos son ejercicios de condicionales

---
+++ app.js
@@ -0,0 +1,27 @@
+//ejercicio 1
+let dato = (true);
+if (dato == true){
+    console.log ("es true");
+}
+else {
+    console.log ("es falso");
+}
+
+//ejercicio 2
+let lenguaje = "javascript";
+if (lenguaje = "javascript"){
+    console.log ("Estoy aprendiendo");
+}
+else {
+    console.log ("Lo aprenderé más adelante");
+}
+
+//ejercicio 3
+function puedePasar(nombre) {
+    if (nombre =="Cosme Fulanito"){
+         return false;
+    }
+    else {
+    return   true;
+    }
+}
